c05948543790e7cbfb715b13b92c464ce0b0f071
rethc <reth@xtra.co.nz>
2021-05-09 05:08:30 +1200

    API key

--- js/config.js
+++ js/config.js
@@ -1,7 +1,5 @@
 //array of Monitor-specific API keys or Main API key to list all monitors
-var __apiKeys = [
-		'u1300251-3de72da725c94f53c7996afb' //TODO set your API
-	];
+var __apiKeys = ['u1300251-3de72da725c94f53c7996afb'];
 
 //refresh interval (in seconds)
 var __refresh = 300; 
